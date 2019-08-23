d592253eca3478fe8cb228a67ca730bc1ea15247
Automedon <42299107+Automedon@users.noreply.github.com>
2019-08-23 07:20:13 +0700

    fix login button on Send Number

---
+++ docs/build/precache-manifest.b3097d1ec59b27007a1f9c1336165d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d98a4ffeebeed11979d7e4080e452e",
+    "url": "/CellPhoneNumber/index.html"
+  },
+  {
+    "revision": "c842b30b3e7458d68b99",
+    "url": "/CellPhoneNumber/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "315d01bbc78edca450eb",
+    "url": "/CellPhoneNumber/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c842b30b3e7458d68b99",
+    "url": "/CellPhoneNumber/static/js/2.b9758f66.chunk.js"
+  },
+  {
+    "revision": "315d01bbc78edca450eb",
+    "url": "/CellPhoneNumber/static/js/main.a41a1f3f.chunk.js"
+  },
+  {
+    "revision": "0578517af01123ec7996",
+    "url": "/CellPhoneNumber/static/js/runtime~main.32e77b83.js"
+  }
+]);
