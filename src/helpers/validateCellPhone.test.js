3f552646c1b0ee7d8596aa2e50acfd79d044a5cc
Valefar React Developer <inzhener.intellect@gmail.com>
2019-08-23 09:30:33 +0700

    and more tests

--- src/helpers/validateCellPhone.test.js
+++ src/helpers/validateCellPhone.test.js
@@ -4,6 +4,19 @@ function rnd10(){
     const allc="0123456789";
     return allc[~~(allc.length*Math.random())];
 }
+function rndc1(){
+    var allc="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_ !@#$%^&*_(),.?|{}[]-=+\\/"
+    return allc[~~(allc.length*Math.random())];
+}
+function genInValid(){
+    let randomLength=()=>{
+        return Math.floor(Math.random() * (1000 - 0)) + 0;}
+    let str=''
+    for (let i=0;i<randomLength();i++){
+        str+=rndc1()
+    }
+    return str
+}
 function genValid(){
     return `\\${rnd10()+rnd10()+rnd10()}\\ ${rnd10()+rnd10()+rnd10()}-${rnd10()+rnd10()}-${rnd10()+rnd10()}`
 }
@@ -28,11 +41,18 @@ describe("Validate CellPhoneNumbers", () => {
   test("valid \\100\\ 111-00-00", () => {
     expect(`\\100\\ 111-00-00`).toMatch(check);
   });
-  test('1000 random tests must be valid ',()=>{
+     test('1000 random tests must be valid ',()=>{
 
-   for (let i=0;i<1000;i++){
-       let value = genValid()
-       console.log(`${value} should match pattern`,!expect(value).toMatch(check))
-       expect(value).toMatch(check)
-   }});
+        for (let i=0;i<1000;i++){
+            let value = genValid()
+            console.log(`${value} should match pattern`,!expect(value).toMatch(check))
+            expect(value).toMatch(check)
+        }});
+    test('1000 random tests must be invalid ',()=>{
+
+        for (let i=0;i<1000;i++){
+            let value = genInValid()
+            console.log(`${value} should not match pattern`,!expect(value).not.toMatch(check))
+            expect(value).not.toMatch(check)
+        }});
 });
