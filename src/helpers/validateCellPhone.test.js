c20e013db6d6b31c0896cc80607556528f5c9c04
Valefar React Developer <inzhener.intellect@gmail.com>
2019-08-23 09:20:58 +0700

    added more tests

--- src/helpers/validateCellPhone.test.js
+++ src/helpers/validateCellPhone.test.js
@@ -1,6 +1,12 @@
 import { validate } from "./validateCellPhone";
 import { check } from "./validateCellPhone";
-
+function rnd10(){
+    const allc="0123456789";
+    return allc[~~(allc.length*Math.random())];
+}
+function genValid(){
+    return `\\${rnd10()+rnd10()+rnd10()}\\ ${rnd10()+rnd10()+rnd10()}-${rnd10()+rnd10()}-${rnd10()+rnd10()}`
+}
 describe("Validate CellPhoneNumbers", () => {
   test("not valid 2", () => {
     expect(validate(2)).toStrictEqual({ number: "Invalid number" });
@@ -22,4 +28,11 @@ describe("Validate CellPhoneNumbers", () => {
   test("valid \\100\\ 111-00-00", () => {
     expect(`\\100\\ 111-00-00`).toMatch(check);
   });
+  test('1000 random tests must be valid ',()=>{
+
+   for (let i=0;i<1000;i++){
+       let value = genValid()
+       console.log(`${value} should match pattern`,!expect(value).toMatch(check))
+       expect(value).toMatch(check)
+   }});
 });
